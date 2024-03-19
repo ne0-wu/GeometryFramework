cc00bf0cf4660500d235b48aab23f2adbda19fbd
neo <ne0.wu@outlook.com>
2024-03-19 13:47:18 +0800

    gaussian quadrature

--- src/PSR.cpp
+++ src/PSR.cpp
@@ -91,6 +91,60 @@ Eigen::Vector3d childCenter(Eigen::Vector3d center, double halfSize, int index)
 	return childCenter;
 }
 
+class GaussianQuadrature
+{
+public:
+	std::vector<double> x;
+	std::vector<double> w;
+
+	GaussianQuadrature(int n = 5)
+	{
+		switch (n)
+		{
+		case 1:
+			x = {0.0};
+			w = {2.0};
+			break;
+		case 2:
+			x = {-0.5773502691896257, 0.5773502691896257};
+			w = {1.0, 1.0};
+			break;
+		case 3:
+			x = {-0.7745966692414834, 0.0, 0.7745966692414834};
+			w = {0.5555555555555556, 0.8888888888888888, 0.5555555555555556};
+			break;
+		case 4:
+			x = {-0.8611363115940526, -0.3399810435848563, 0.3399810435848563, 0.8611363115940526};
+			w = {0.3478548451374538, 0.6521451548625461, 0.6521451548625461, 0.3478548451374538};
+			break;
+		case 5:
+			x = {-0.9061798459386640, -0.5384693101056831, 0.0, 0.5384693101056831, 0.9061798459386640};
+			w = {0.2369268850561891, 0.4786286704993665, 0.5688888888888889, 0.4786286704993665, 0.2369268850561891};
+			break;
+		case 6:
+			x = {-0.9324695142031521, -0.6612093864662645, -0.2386191860831969, 0.2386191860831969, 0.6612093864662645, 0.9324695142031521};
+			w = {0.1713244923791704, 0.3607615730481386, 0.4679139345726910, 0.4679139345726910, 0.3607615730481386, 0.1713244923791704};
+			break;
+		case 7:
+			x = {-0.9491079123427585, -0.7415311855993945, -0.4058451513773972, 0.0, 0.4058451513773972, 0.7415311855993945, 0.9491079123427585};
+			w = {0.1294849661688697, 0.2797053914892766, 0.3818300505051189, 0.4179591836734694, 0.3818300505051189, 0.2797053914892766, 0.1294849661688697};
+			break;
+		case 8:
+			x = {-0.9602898564975363, -0.7966664774136267, -0.5255324099163290, -0.1834346424956498, 0.1834346424956498, 0.5255324099163290, 0.7966664774136267, 0.9602898564975363};
+			w = {0.1012285362903763, 0.2223810344533745, 0.3137066458778873, 0.3626837833783620, 0.3626837833783620, 0.3137066458778873, 0.2223810344533745, 0.1012285362903763};
+			break;
+		case 9:
+			x = {-0.9681602395076261, -0.8360311073266358, -0.6133714327005904, -0.3242534234038089, 0.0, 0.3242534234038089, 0.6133714327005904, 0.8360311073266358, 0.9681602395076261};
+			w = {0.0812743883615744, 0.1806481606948574, 0.2606106964029354, 0.3123470770400029, 0.3302393550012598, 0.3123470770400029, 0.2606106964029354, 0.1806481606948574, 0.0812743883615744};
+			break;
+		case 10:
+			x = {-0.9739065285171717, -0.8650633666889845, -0.6794095682990244, -0.4333953941292472, -0.1488743389816312, 0.1488743389816312, 0.4333953941292472, 0.6794095682990244, 0.8650633666889845, 0.9739065285171717};
+			w = {0.0666713443086881, 0.1494513491505806, 0.2190863625159820, 0.2692667193099963, 0.2955242247147529, 0.2955242247147529, 0.2692667193099963, 0.2190863625159820, 0.1494513491505806, 0.0666713443086881};
+			break;
+		}
+	}
+};
+
 struct Node
 {
 	// Node box
@@ -229,6 +283,9 @@ struct Node
 		double element = 0.0;
 		if (intersection.isIntersecting)
 		{
+			// Integrate base function product over the intersection
+			Eigen::Vector3d min = intersection.min;
+			Eigen::Vector3d max = intersection.max;
 		}
 
 		return 0;
@@ -419,14 +476,77 @@ private:
 	}
 };
 
+void testGaussianQuadrature(int n)
+{
+	std::cout << "Testing Gauss Quadrature with " << n << " points" << std::endl;
+
+	GaussianQuadrature gauss(n);
+	auto x = gauss.x;
+	auto w = gauss.w;
+
+	// Accurate results
+	double sinAccurate = 0.0;							 // Accurate result of integrating sin(x) from -1 to 1
+	double cosAccurate = std::sin(1.0) - std::sin(-1.0); // Accurate result of integrating cos(x) from -1 to 1
+	double xAccurate = 0.0;								 // Accurate result of integrating x from -1 to 1
+	double x2Accurate = 2.0 / 3.0;						 // Accurate result of integrating x^2 from -1 to 1
+	double x3Accurate = 0.0;							 // Accurate result of integrating x^3 from -1 to 1
+	double expAccurate = std::exp(1.0) - std::exp(-1.0); // Accurate result of integrating e^x from -1 to 1
+
+	// Test with annoying function
+	auto test = [&x, &w](std::function<double(double)> f, double accurate)
+	{
+		double result = 0.0;
+		for (int i = 0; i < x.size(); i++)
+			result += w[i] * f(x[i]);
+		double logError = std::log(std::abs(result - accurate)) / std::log(10);
+		std::cout << "Result: " << result << " (Error: " << logError << ")" << std::endl;
+	};
+
+	// Test with sin
+	test([](double x)
+		 { return std::sin(x); },
+		 sinAccurate);
+
+	// Test with cos
+	test([](double x)
+		 { return std::cos(x); },
+		 cosAccurate);
+
+	// Test with x
+	test([](double x)
+		 { return x; },
+		 xAccurate);
+
+	// Test with x^2
+	test([](double x)
+		 { return x * x; },
+		 x2Accurate);
+
+	// Test with x^3
+	test([](double x)
+		 { return x * x * x; },
+		 x3Accurate);
+
+	// Test with e^x
+	test([](double x)
+		 { return std::exp(x); },
+		 expAccurate);
+}
+
 void testOctree(PointCloud pointCloud)
 {
-	Octree tree(pointCloud, Eigen::Vector3d::Zero(), 1.0);
-	tree.printDepthFirst();
 
-	tree.refine();
+	for (int n : {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+	{
+		testGaussianQuadrature(n);
+	}
 
-	tree.printDepthFirst();
+	// Octree tree(pointCloud, Eigen::Vector3d::Zero(), 1.0);
+	// tree.printDepthFirst();
 
-	tree.printBreadthFirst();
+	// tree.refine();
+
+	// tree.printDepthFirst();
+
+	// tree.printBreadthFirst();
 }
